0c0321d6e1ca5ad1d115b9929f3ac371021e2de6
siidom <99956085+siidom@users.noreply.github.com>
2023-04-09 16:02:08 +0530

    Add files via upload

---
+++ CacheMatrix.R
@@ -0,0 +1,51 @@
+makeCacheMatrix <- function( m = matrix() ) {
+  
+  i <- NULL
+  
+  set <- function( matrix ) {
+    m <<- matrix
+    i <<- NULL
+  }
+  
+  
+  get <- function() {
+    
+    m
+  }
+  
+  
+  setInverse <- function(inverse) {
+    i <<- inverse
+  }
+  
+  
+  getInverse <- function() {
+    
+    i
+  }
+  
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
+
+
+
+cacheSolve <- function(x, ...) {
+  
+  m <- x$getInverse()
+  
+  
+  if( !is.null(m) ) {
+    message("reading cache..")
+    return(m)
+  }
+  
+  data <- x$get()
+  
+  m <- solve(data) %*% data
+  
+  x$setInverse(m)
+  
+  m
+}
